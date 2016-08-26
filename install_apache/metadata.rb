aeb8703d940f17ac14e807dec29636da84661f43
snehithakavuri <snehitha.kavuri@gmail.com>
2016-08-26 13:08:38 -0400

    modified metadata.rb

--- install_apache/metadata.rb
+++ install_apache/metadata.rb
@@ -6,3 +6,4 @@ description      'Installs/Configures install_apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends "apt"
+depends 'iptables','1.1.0'
