fef6244a2e19da6774731b821253d36414fb76ea
snehithakavuri <snehitha.kavuri@gmail.com>
2016-08-26 22:03:08 -0400

    jboss installation and metadata file

--- install_apache/metadata.rb
+++ install_apache/metadata.rb
@@ -7,5 +7,3 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends "apt"
 depends "iptables"
-cookbook 'java', '~> 1.42.0'
-cookbook 'jboss7', '~> 1.0.0'
